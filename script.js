2dc8871cd37a80a893dea5bbcaeec75ec08e8bb8
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-04-23 16:40:29 +0100

    Create functions to update and limit display
    Implement number input functions, limited to display size,
    as well as a square root function. Created rounding function
    to deal with long decimals and large numbers.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const DISPLAY_SIZE = 8;
+
 let display = document.querySelector("#display");
 let digitButtons = document.querySelectorAll(".number-button");
 let operatorButtons = document.querySelectorAll(".operator-button");
@@ -9,10 +11,66 @@ let equalsButton = document.querySelector("#equals-button");
 let previous = "";
 let operation = "";
 
+for (let digit of digitButtons) {
+    digit.addEventListener("click", updateDisplay);
+}
+backButton.addEventListener("click", updateDisplay);
+for (let operator of operatorButtons) {
+    operator.addEventListener("click", calculateOperation);
+}
+sqrtButton.addEventListener("click", calculateSqrt);
+equalsButton.addEventListener("click", calculateOperation);
+clearButton.addEventListener("click", clearMemory);
 
 function updateDisplay (event) {
-    event.target.textContent
+    let currentDisplay = display.textContent;
+    let buttonText = event.target.textContent;
+    
+    switch (buttonText) {
+        case "±":
+            display.textContent = -currentDisplay;
+            break;
+        case ".":
+            if (currentDisplay.length > DISPLAY_SIZE) {
+                break;
+            } else if (display.textContent.includes(".")) {
+                break;
+            } else {
+                display.textContent = currentDisplay + ".";
+                break;
+            }
+        case "⟵":
+            display.textContent = currentDisplay.slice(0, currentDisplay.length-1);
+            break;
+        default:
+            if (currentDisplay.length > DISPLAY_SIZE) {
+                break;
+            } else {
+                display.textContent = currentDisplay + buttonText;
+                break;
+            }
+    }
 }
+
+function calculateOperation (event) {
+    let operation = event.target.textContent;
+    
+}
+
+function calculateSqrt () {
+    if (+display.textContent >= 0) {
+        display.textContent = roundToDisplaySize(+Math.sqrt(+display.textContent));
+    } else {
+        display.textContent = "ERROR";
+    }
+}
+
+function clearMemory () {
+    display.textContent = "";
+    previous = "";
+    operation = "";
+}
+
 
 function operate (a, b, operator) {
     switch (operator) {
@@ -37,13 +95,16 @@ function operate (a, b, operator) {
     }
 }
 
-function squareRoot(a) {
-    if (a >= 0) {
-        return Math.sqrt(a);
+function roundToDisplaySize(number) {
+    let numberAsString = number.toString();
+    let decimalPosition = numberAsString.indexOf(".");
+    if (decimalPosition === -1 && numberAsString.length > DISPLAY_SIZE) {
+        return number.toExponential(3);
+    } else if (decimalPosition !== -1 && numberAsString.length > DISPLAY_SIZE) {
+        return number.toFixed(DISPLAY_SIZE - decimalPosition);
     } else {
-        return "ERROR";
+        return number;
     }
-}
-
+  }
 
 
