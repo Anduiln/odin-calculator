bdf33b990d990a9160e4fff811f400aacb15700d
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-04-24 16:44:05 +0100

    Implement calculations and clear functions
    Create function to update display and memory correctly for
    all operations, as well as to clear all memory.
    Update to use currentValue variable to ensure display can remain
    displayed while resetting calculations when "=" pressed.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DISPLAY_SIZE = 8;
+const DISPLAY_SIZE = 7;
 
 let display = document.querySelector("#display");
 let digitButtons = document.querySelectorAll(".number-button");
@@ -8,7 +8,8 @@ let backButton = document.querySelector("#back-button");
 let clearButton = document.querySelector("#clear-button");
 let equalsButton = document.querySelector("#equals-button");
 
-let previous = "";
+let currentValue = "";
+let previousValue = "";
 let operation = "";
 
 for (let digit of digitButtons) {
@@ -23,51 +24,76 @@ equalsButton.addEventListener("click", calculateOperation);
 clearButton.addEventListener("click", clearMemory);
 
 function updateDisplay (event) {
-    let currentDisplay = display.textContent;
     let buttonText = event.target.textContent;
     
     switch (buttonText) {
         case "±":
-            display.textContent = -currentDisplay;
+            currentValue = -currentValue;
+            display.textContent = currentValue;
             break;
         case ".":
-            if (currentDisplay.length > DISPLAY_SIZE) {
+            if (currentValue.length > DISPLAY_SIZE) {
                 break;
             } else if (display.textContent.includes(".")) {
                 break;
             } else {
-                display.textContent = currentDisplay + ".";
+                currentValue = currentValue + ".";
+                display.textContent = currentValue;
                 break;
             }
         case "⟵":
-            display.textContent = currentDisplay.slice(0, currentDisplay.length-1);
+            currentValue = currentValue.slice(0, currentValue.length-1);
+            display.textContent = currentValue;
             break;
         default:
-            if (currentDisplay.length > DISPLAY_SIZE) {
+            if (currentValue.length > DISPLAY_SIZE) {
                 break;
+            } else if (currentValue === "" || currentValue === "ERROR") {
+                currentValue = buttonText;
+                display.textContent = currentValue;
             } else {
-                display.textContent = currentDisplay + buttonText;
+                currentValue = currentValue + buttonText;
+                display.textContent = currentValue;
                 break;
             }
     }
 }
 
 function calculateOperation (event) {
-    let operation = event.target.textContent;
-    
+    if (previousValue === "" && operation === "") {
+        previousValue = currentValue;
+        currentValue = "";
+        operation = (event.target.textContent === "=") ? "" : event.target.textContent;
+    } else if (operation === "") {
+        currentValue = "";
+        operation = event.target.textContent;
+    } else {
+        display.textContent = 
+                roundToDisplaySize(operate(+previousValue, +currentValue, operation));
+        previousValue = display.textContent;
+        currentValue = "";
+        operation = (event.target.textContent === "=") ? "" : event.target.textContent;
+    }
 }
 
 function calculateSqrt () {
     if (+display.textContent >= 0) {
         display.textContent = roundToDisplaySize(+Math.sqrt(+display.textContent));
+        currentValue = "";
+        previousValue = display.textContent;
+        operation = "";
     } else {
-        display.textContent = "ERROR";
+        currentValue = "ERROR";
+        display.textContent = currentValue;
+        previousValue = "";
+        operation = "";
     }
 }
 
 function clearMemory () {
-    display.textContent = "";
-    previous = "";
+    currentValue = "";
+    display.textContent = "0";
+    previousValue = "";
     operation = "";
 }
 
@@ -81,7 +107,7 @@ function operate (a, b, operator) {
             return a - b;
             break;
         case "÷":
-            return a / b;
+            return (b === 0)? "ERROR" : a / b;
             break;
         case "×":
             return a * b;
